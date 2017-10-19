fd552b87cfe159ff408170a262ec68a27d7ce75e
Giugiu19 <gpirasking@outlook.it>
2017-10-19 17:03:09 +0200

    zero: Update Blobs to Nougat

--- Android.mk
+++ Android.mk
@@ -1,11 +1,10 @@
-# Copyright (C) 2015 The CyanogenMod Project
-# Copyright (C) 2015 The Android Open Kang Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,3 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter  zerofltexx  zeroltexx,$(TARGET_DEVICE)),)
+
+endif
